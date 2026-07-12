1698ef5f24b874c2eb869a687f7c42ff6d527b40
Thanishka <thanishka0211@gmail.com>
2026-07-12 23:53:21 +0530

    Updated frontend API URLs

--- frontend/script.js
+++ frontend/script.js
@@ -20,13 +20,13 @@ if (registerForm) {
 
         try {
 
-            const response = await fetch("http://localhost:5000/api/users/register", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify(user)
-            });
+            const response = await fetch("https://research-ethics-portal-v2.onrender.com/api/users/register", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify(user)
+});
 
             const data = await response.json();
 
@@ -87,7 +87,7 @@ if (loginForm) {
 
         try {
 
-            const response = await fetch("http://localhost:5000/api/users/login", {
+            const response = await fetch("https://research-ethics-portal-v2.onrender.com/api/users/login", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
