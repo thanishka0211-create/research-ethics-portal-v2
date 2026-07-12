ddacc7eca28229358ec069727416ca1ac16bf46e
Thanishka <thanishka0211@gmail.com>
2026-07-13 00:31:30 +0530

    Connected Railway MySQL

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,19 +1,20 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "thanishka0211",
-    database: "research_ethics_portal"
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT
 });
 
 db.connect((err) => {
-    if (err) {
-        console.log("Database Connection Failed!");
-        console.log(err);
-    } else {
-        console.log("MySQL Connected Successfully!");
-    }
+  if (err) {
+    console.log("Database Connection Failed!");
+    console.log(err);
+  } else {
+    console.log("MySQL Connected Successfully!");
+  }
 });
 
 module.exports = db;
