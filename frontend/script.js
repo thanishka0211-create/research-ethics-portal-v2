b44bc07319cdba4db13f40c82f791a8a3143de4a
Thanishka <thanishka0211@gmail.com>
2026-07-19 15:03:18 +0530

    Updated API URLs to Render

--- frontend/script.js
+++ frontend/script.js
@@ -89,7 +89,7 @@ if (loginForm) {
 
         try {
 
-            const response = await fetch("http://localhost:5000/api/users/login", {
+            const response = await fetch("https://research-ethics-portal-v2.onrender.com/api/users/login", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
